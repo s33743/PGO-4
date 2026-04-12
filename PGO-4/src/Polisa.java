07a81fec22303f869fafb0329d59209e076a13e2
s33743 <s33743@pjwstk.edu.pl>
2026-04-12 14:32:55 +0200

    Dodałem Klasę Polisa

--- PGO-4/src/Polisa.java
+++ PGO-4/src/Polisa.java
@@ -1,6 +1,8 @@
+import java.util.Objects;
+
 public class Polisa {
     private String numerPolisy;
-    private String klient;
+    private String nazwaKlienta;
     private double skladkaBazowa;
     private int poziomRyzyka;
     private double wartoscPojazdu;
@@ -10,16 +12,24 @@ public class Polisa {
     private static int liczbaUtworzonychPolis = 0;
     private static final double OPLATA_ADMINISTRACYJNA = 50.0;
 
-    public Polisa(String numerPolisy, String klient, double skladkaBazowa, int poziomRyzyka, double wartoscPojazdu, boolean czyMaAlarm, boolean czyBezszkodowyKlient) {
+    public Polisa(
+            String numerPolisy,
+            String nazwaKlienta,
+            double skladkaBazowa,
+            int poziomRyzyka,
+            double wartoscPojazdu,
+            boolean czyMaAlarm,
+            boolean czyBezszkodowyKlient
+    ) {
         this.numerPolisy = numerPolisy;
-        this.klient = klient;
+        this.nazwaKlienta = nazwaKlienta;
         this.skladkaBazowa = skladkaBazowa;
         this.poziomRyzyka = poziomRyzyka;
         this.wartoscPojazdu = wartoscPojazdu;
         this.czyBezszkodowyKlient = czyBezszkodowyKlient;
+        this.czyMaAlarm = czyMaAlarm;
 
         liczbaUtworzonychPolis++;
-
     }
 
     public String getNumerPolisy() {
@@ -27,41 +37,91 @@ public class Polisa {
     }
 
     public double obliczSkladkeKoncowa() {
-        double wynik = skladkaBazowa + OPLATA_ADMINISTRACYJNA;
-        double obecna = obliczSkladkeKoncowa();
-        double nowa = obecna;
+        double skladka = skladkaBazowa + OPLATA_ADMINISTRACYJNA;
 
-        // dopłata za ryzyko
-        wynik += poziomRyzyka * 120;
+        skladka += poziomRyzyka * 120;
 
-        // dopłata za drogi pojazd
         if (wartoscPojazdu > 60000) {
-            wynik += 150;
+            skladka += 200;
         }
 
-        // zniżki
         if (czyMaAlarm) {
-            wynik *= 0.95;
+            skladka -= 100;
         }
 
         if (czyBezszkodowyKlient) {
-            wynik *= 0.92;
-
+            skladka *= 0.9;
         }
 
-        // minimalna skladka
-        double min = obecna * 0.90;
-        if (nowa < min) {
-            nowa = min;
+        if (skladka < skladkaBazowa) {
+            skladka = skladkaBazowa;
         }
 
-        // maksymalna skladka
-        double max = obecna * 1.25;
-        if (nowa > max) {
-            nowa = max;
+        return Math.round(skladka * 100.0) / 100.0;
+    }
+
+    public double obliczSkladkeOdnowieniowa() {
+        double bazowa = obliczSkladkeKoncowa();
+        double nowa = bazowa;
+
+        if (poziomRyzyka == 4) {
+            nowa *= 1.10;
+        }
+        else if (poziomRyzyka >= 5) {
+            nowa *= 1.20;
+        }
+
+        if (wartoscPojazdu > 60000) {
+            nowa += 150;
+        }
+
+        if (czyBezszkodowyKlient) {
+            nowa *= 0.92;
+        }
+
+        if (czyMaAlarm) {
+            nowa *= 0.95;
+        }
+
+        double skladkaMin = bazowa * 0.9;
+        double skladkaMax = bazowa * 1.25;
+
+        if (nowa < skladkaMin) {
+            nowa = skladkaMin;
+        }
+
+        if (nowa > skladkaMax) {
+            nowa = skladkaMax;
+        }
+
+        return Math.round(nowa * 100.0) / 100.0;
+    }
+
+    public String pobierzPodsumowanieRyzyka() {
+        if (poziomRyzyka >= 4) {
+            return "Wysokie Ryzyko";
+        } else if (poziomRyzyka == 3) {
+            return "Srednie Ryzyko";
+        } else {
+            return "Niskie Ryzyko";
         }
     }
 
+    public static int pobierzLiczbeUtworzonychPolis() {
+        return liczbaUtworzonychPolis;
+    }
 
+    public String toString() {
+        return "Polisa: " + numerPolisy + ", klient: " + nazwaKlienta + ", skladkaKoncowa: " + obliczSkladkeKoncowa();
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Polisa)) return false;
+        Polisa polisa = (Polisa) o;
+        return Objects.equals(numerPolisy, polisa.numerPolisy);
+    }
 }
+
+
 
